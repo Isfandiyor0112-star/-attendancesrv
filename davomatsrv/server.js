bf287a2862e185cc3adceba21f5581002370b471
Isfandiyor0112-star <imamalievisfandier@gmail.com>
2025-10-30 20:05:00 +0500

    server.js

--- davomatsrv/server.js
+++ davomatsrv/server.js
@@ -37,23 +37,23 @@ const users = [
   { login: "22maktab", password: "fotima", name: "Djamalova.F.A.", className: "3D" },
   { login: "22maktab", password: "kimmat", name: "Kambarova.K.M.", className: "4A" },
   { login: "22maktab", password: "vera", name: "Polyakova.V.A.", className: "4B" },
-  { login: "22maktab", password: "dilfuza", name: "Normuratova.D.X.", className: "4V" },
+  { login: "22maktab", password: "dilfuza2", name: "Normuratova.D.X.", className: "4V" },
   { login: "22maktab", password: "sevara", name: "Madaminova.S.Y.", className: "4G" },
   { login: "22maktab", password: "dilafruz", name: "Sheranova.D.T.", className: "4D" },
-  { login: "22maktab", password: "gulnara", name: "Zokirxonova.G.B.", className: "5A" },
+  { login: "22maktab", password: "gulnara22", name: "Zokirxonova.G.B.", className: "5A" },
   { login: "22maktab", password: "xilola", name: "Abdumavlonova.X.M.", className: "5B" },
-  { login: "22maktab", password: "xilola", name: "Ermatova.X.A.", className: "5V" },
+  { login: "22maktab", password: "xilola22", name: "Ermatova.X.A.", className: "5V" },
   { login: "22maktab", password: "orzigul", name: "Mamatqulova.O.S.", className: "5G" },
   { login: "22maktab", password: "rustam", name: "Raximov.R.R.", className: "6A" },
   { login: "22maktab", password: "avazjon", name: "Ismoilov.A.K.", className: "6B" },
-  { login: "22maktab", password: "dilafruz", name: "Yettiyeva.D.M.", className: "6V" },
+  { login: "22maktab", password: "dilafruz22", name: "Yettiyeva.D.M.", className: "6V" },
   { login: "22maktab", password: "barno", name: "Malikova.B.A.", className: "6G" },
   { login: "22maktab", password: "gozal", name: "Normatova.G.D.", className: "6D" },
   { login: "22maktab", password: "natasha", name: "Nefyodova.N.A.", className: "7A" },
-  { login: "22maktab", password: "dilfuza", name: "Xakimova.D.A.", className: "7B" },
+  { login: "22maktab", password: "dilfuza3", name: "Xakimova.D.A.", className: "7B" },
   { login: "22maktab", password: "inomjon", name: "Fozilov.I.O.", className: "7V" },
   { login: "22maktab", password: "viktoriya", name: "Buligina.V.Y.", className: "8A" },
-  { login: "22maktab", password: "matluba", name: "Yardamova.M.M.", className: "8B" },
+  { login: "22maktab", password: "matluba22", name: "Yardamova.M.M.", className: "8B" },
   { login: "22maktab", password: "orif", name: "Mandiyev.O.A.", className: "8V" },
   { login: "22maktab", password: "nigora", name: "Pardayeva.N.M.", className: "9A" },
   { login: "22maktab", password: "alisher", name: "Aripov.A.I.", className: "9B" },
@@ -61,16 +61,17 @@ const users = [
   { login: "22maktab", password: "asom", name: "Xodjahanov.A.O.", className: "9G" },
   { login: "22maktab", password: "mehriniso", name: "Ismoilova.M.A.", className: "9D" },
   { login: "22maktab", password: "olesya", name: "Xasanova.O.G.", className: "10A" },
-  { login: "22maktab", password: "dilafruz", name: "Satimova.D.F.", className: "10B" },
+  { login: "22maktab", password: "dilafruz3", name: "Satimova.D.F.", className: "10B" },
   { login: "22maktab", password: "shahodat", name: "Ruzmatova.S.M.", className: "10V" },
   { login: "22maktab", password: "marguba", name: "Baltabayeva.M.T.", className: "11A" },
   { login: "22maktab", password: "svetlana", name: "Ryabinina.S.Y.", className: "11B" },
   { login: "22maktab", password: "maftuna", name: "Abdullayeva.M.R.", className: "11V" },
-  { login: "22maktab", password: "nilufar", name: "Aliyeva.N.M.", className: "11G" },
+  { login: "22maktab", password: "nilufar22", name: "Aliyeva.N.M.", className: "11G" },
   { login: "shaxnoza", password: "22_admin", name: "Ruzimatova.Sh.R" },
   { login: "furkat", password: "diamondkey", name: "Abduraxmonov.F.N" },
-  { login: "matlyuba", password: "ironkey", name: "Abdunamatova.M" },
-  { login: "admin", password: "goldenkey", name: "Bayjanova.Sh" }
+  { login: "matlyuba", password: "ironkey", name: "Abdunamatova.M"},
+  { login: "bayjanova", password: "goldenkey", name: "Bayjanova.Sh"}
+   
 ];
 
 app.use(cors());
@@ -164,6 +165,7 @@ app.get('/api/ping-tg', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
    });
+
 
 
 
